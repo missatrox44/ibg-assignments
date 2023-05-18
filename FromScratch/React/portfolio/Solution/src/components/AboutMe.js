7d245c12b8726d653e8bdda955809e09213cde0f
Sara Baqla <missatrox44@gmail.com>
2023-05-18 13:08:39 -0500

    started react portfolio challenge

---
+++ FromScratch/React/portfolio/Solution/src/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      {/* Add more information about you here */}
+    </div>
+  );
+}
+
+export default AboutMe;
