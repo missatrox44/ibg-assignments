a12dfd720c4267a5d4b701a0a03a6923886bd84c
Sara Baqla <missatrox44@gmail.com>
2023-07-04 20:06:33 -0500

    updated DOM instructions

---
+++ InClass/DOM/README.md
@@ -0,0 +1,10 @@
+# DOM Example
+Form will allow students to enter their name and a color
+When the form is submitted, the page's background color will change to the color they entered, and a welcome message will be displayed with their name.
+
+
+## Teaching instructions
+- Open VS Code Editor
+- Launch live server
+- Open Dev Tools
+- Manipulate logic form && to || and demonstrate difference
--- InClass/DOM/script.js
+++ InClass/DOM/script.js
@@ -8,3 +8,10 @@ document.getElementById('form').addEventListener('submit', function(e) {
       document.getElementById('message').innerText = "Hello, " + name + "! Welcome to your " + color + " page!";
   }
 });
+
+
+// We use document.getElementById() to select our form and attach an event listener to it. This function runs every time the form is submitted.
+// Event listener takes in two parameters: type of event as a string, callback function
+// We prevent the default form submission behavior, which would refresh the page, using e.preventDefault().
+// We grab the name and color from the form inputs using their id and value.
+// If both a name and a color have been entered, we change the background color of the page to the entered color and display a message including the name and color entered.
